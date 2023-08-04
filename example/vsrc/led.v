299d75b59042a2a0dbcb800a115a29db2e91c695
ZQS <bluesky@bluepat.cn>
2023-08-05 02:00:45 +0800

    将流水灯接入NVBoard

--- example/vsrc/led.v
+++ example/vsrc/led.v
@@ -5,14 +5,14 @@ module led(
   output [15:0] ledr
 );
   reg [31:0] count;
-  reg [7:0] led;
+  reg [15:0] led;
   always @(posedge clk) begin
     if (rst) begin led <= 1; count <= 0; end
     else begin
-      if (count == 0) led <= {led[6:0], led[7]};
+      if (count == 0) led <= {led[14:0], led[15]};
       count <= (count >= 5000000 ? 32'b0 : count + 1);
     end
   end
 
-  assign ledr = {led, sw};
+  assign ledr = led;
 endmodule
